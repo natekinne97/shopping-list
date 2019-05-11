5a87178e529aaa2b9e9fcac4712f11c4f7ac8bb4
Nate Kinne <nathan.kinne@gmail.com>
2019-05-11 00:57:21 -0500

    Initial commit

--- code.js
+++ code.js
@@ -1,10 +1,24 @@
+//delete item
 function del() {
     $('.shopping-item-delete').click(function (event) {
-        console.log("delete clicked");
-        const del = $(this);
-        const par = $(this).parent();
-        console.log(par);
+        $(this).closest('li').remove();
+    });
+}
+//check off item
+function check(){
+    $('.shopping-item-toggle').click(function(event){
+        $(this).closest('li').find('.shopping-item').toggleClass('shopping-item__checked');
     });
 }
 
+function add(){
+    $('#js-shopping-list-form').submit(event => {
+        // this stops the default form submission behavior
+        event.preventDefault();
+        console.log('worked');
+    });  
+}
+
+
 $(del);
+$(check);
